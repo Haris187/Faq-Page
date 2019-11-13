4215ddb807b7d79bc0868da1d9485ab5165e440b
Haris187 <hary2k121@gmail.com>
2019-11-13 23:11:03 +0000

    added links

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Header() {
     return (
         <header>
-        <h1>FAQ page</h1>
+        <h1>FAQs.</h1>
         </header>
     )
-}
+  }
+
 
 export default Header
